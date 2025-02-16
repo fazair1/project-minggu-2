f67c10c3a40b54b690e9c504f2dca002f10aa29e
fazair1 <fauzan.irfanto1@gmail.com>
2025-02-16 21:46:44 +0700

    feat: read and update feature

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -18,12 +18,14 @@ public class Main {
         int menu=0;
 
         while (menu!=5) {
+            studentList = studentDAO.getAllStudents();
 
+            System.out.println();
             System.out.println("=====Student Management System=====");
             System.out.println("1. Add Student");
-            System.out.println("2. Read Product");
-            System.out.println("3. Update Product");
-            System.out.println("4. Delete Product");
+            System.out.println("2. Read Student");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
             System.out.println("5. Exit");
             System.out.println("=====-=====-=====-=====-=====-=====");
 
@@ -45,17 +47,17 @@ public class Main {
                     createStudent(studentList, studentDAO);
                     break;
 
-//                case 2:
-//                    readProduct(listProduk);
-//                    break;
+                case 2:
+                    readStudent(studentList, studentDAO);
+                    break;
 //
-//                case 3:
-//                    updateProduct(listProduk);
-//                    break;
+                case 3:
+                    updateStudent(studentList, studentDAO);
+                    break;
 //
-//                case 4:
-//                    deleteProduct(listProduk);
-//                    break;
+                case 4:
+                    deleteProduct(listProduk);
+                    break;
 
                 case 5:
                     return;
@@ -70,8 +72,8 @@ public class Main {
         String studentMajor;
         double studentGpa = 0.0;
 
-        System.out.println("=====-=====Create Student====-=====");
-//        System.out.println("=====-=====-=====-=====-=====-=====");
+        System.out.println("==========Create Student=========");
+//        System.out.println("=====-=====-=====-=====-=====-===");
 
         do {
             System.out.print("Insert Student Name [Not Nullable]: ");
@@ -121,10 +123,10 @@ public class Main {
             }
         } while (gpa.isEmpty() || studentGpa <= 0);
 
-        Student newStudent = new Student((studentList.size()), studentname, studentEmail, studentAge, studentMajor, studentGpa);
+        Student newStudent = new Student((studentList.size()+1), studentname, studentEmail, studentAge, studentMajor, studentGpa);
         studentDAO.addStudent(newStudent);
 
-        System.out.println("Successfully created Student with data:");
+        System.out.println("Successfully updated Student with data:");
         System.out.println("ID: "+newStudent.getId());
         System.out.println("Name: "+newStudent.getName());
         System.out.println("Email: "+newStudent.getEmail());
@@ -134,42 +136,125 @@ public class Main {
     }
     static void readStudent (List<Student> studentList, StudentDAO studentDAO) {
 //        System.out.println("=====-=====-=====-=====-=====-=====");
-        System.out.println("=====-=====Read Student======-=====");
+        System.out.println("===========Read Student============");
+        System.out.println();
         Scanner scan = new Scanner(System.in);
         String temp;
 
         if (studentList.isEmpty()) {
-            System.out.println("Data kosong");
+            System.out.println("Empty Data");
             return;
         }
-        showProductList(listProduk);
+        showProductList(studentList, studentDAO);
         do {
-            System.out.print("Apakah anda ingin melakukan sorting? [y/n]: ");
+            System.out.print("Press x to exit read student [x]: ");
             temp = scan.nextLine();
             temp = temp.trim();
-        }while (!(temp.equals("y") || temp.equals("n")));
+        }while (!(temp.equals("x")));
+    }
+    static void updateStudent (List<Student> studentList, StudentDAO studentDAO) {
+//        System.out.println("=====-=====-=====-=====-=====-=====");
+        System.out.println("==========Update Student===========");
+        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        String num;
+        boolean produkFound;
+        String temp;
+        int ID = 0;
 
-        if (temp.equals("n")) {
+        if (studentList.isEmpty()) {
+            System.out.println("Empty Data");
             return;
         }
-        else {
-            do {
-                System.out.print("Apakah sorting berdasarkan nama, harga, atau kategori? [nama/harga/kategori]: ");
-                temp = scan.nextLine();
-                temp = temp.trim();
+        showProductList(studentList, studentDAO);
 
-            }while (!(temp.equals("nama") || temp.equals("harga") || temp.equals("kategori")));
-            if (temp.equals("nama")) {
-                Collections.sort(listProduk, new SortbyName());
-            } else if (temp.equals("harga")) {
-                Collections.sort(listProduk, new SortbyPrice());
-            } else if (temp.equals("kategori")) {
-                Collections.sort(listProduk, new SortbyCategory());
+        do {
+            System.out.print("Insert Student ID [1-"+studentList.size()+"]: ");
+            num = scan.nextLine();
+            num = num.trim();
+
+            if (onlyDigits(num) && !(num.isEmpty())) {
+                ID = Integer.parseInt(num);
             }
-            System.out.println("Sorting berhasil!");
-        }
-    }
+            try {
+                studentDAO.searchStudentByID(num);
+                //  Block of code to try
+            }
+            catch(Exception e) {
+                System.out.println("Student Doesnt exist");
+                //  Block of code to handle errors
+            }
 
+        } while (!(onlyDigits(num)) || num.isEmpty() || (ID<=0 || ID>studentList.size()) );
+
+        String studentname;
+        String studentEmail;
+        int studentAge = 0;
+        String studentMajor;
+        double studentGpa = 0.0;
+
+        System.out.println("==========Update Student=========");
+//        System.out.println("=====-=====-=====-=====-=====-===");
+
+        do {
+            System.out.print("Insert Student Name [Not Nullable]: ");
+            studentname = scan.nextLine();
+            studentname = studentname.trim();
+
+        } while (studentname.isEmpty());
+
+        do {
+            System.out.print("Insert Student Email [Not Nullable]: ");
+            studentEmail = scan.nextLine();
+            studentEmail = studentEmail.trim();
+
+        } while (studentEmail.isEmpty());
+
+        String age;
+        do {
+            System.out.print("Insert Student Age [Not Nullable|Is Digit|Age > 0]: ");
+            age = scan.nextLine();
+            age = age.trim();
+
+            if (onlyDigits(age) && !(age.isEmpty())) {
+                studentAge = Integer.parseInt(age);
+            }
+        } while (age.isEmpty() || !(onlyDigits(age)) || studentAge <= 0);
+
+        do {
+            System.out.print("Insert Student Major [Not Nullable]: ");
+            studentMajor = scan.nextLine();
+            studentMajor = studentMajor.trim();
+
+        } while (studentMajor.isEmpty());
+
+        String gpa;
+        do {
+            System.out.print("Insert Student GPA [Not Nullable|Is Digit|GPA > 0]: ");
+            gpa = scan.nextLine();
+            gpa = gpa.trim();
+
+            try
+            {
+                studentGpa = Double.parseDouble(gpa);
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Please input a double!");
+            }
+        } while (gpa.isEmpty() || studentGpa <= 0);
+
+        Student updateStudent = new Student((studentList.size()), studentname, studentEmail, studentAge, studentMajor, studentGpa);
+        studentDAO.updateStudent(updateStudent);
+
+        System.out.println("Successfully created Student with data:");
+        System.out.println("ID: "+updateStudent.getId());
+        System.out.println("Name: "+updateStudent.getName());
+        System.out.println("Email: "+updateStudent.getEmail());
+        System.out.println("Age: "+updateStudent.getAge());
+        System.out.println("Major: "+updateStudent.getMajor());
+        System.out.println("GPA: "+updateStudent.getGpa());
+    }
     static boolean onlyDigits(String s) {
 
         // Traverse each character in the string
@@ -183,6 +268,16 @@ public class Main {
             }
         }
         return true;  // If all characters are digits, return true
+    }
+    static void showProductList (List<Student> studentList, StudentDAO studentDAO) {
+
+        System.out.println("=ID=|======Name=====|=============Email============|=Age=|========Major=======|=GPA=|");
+        for (Student student : studentList) {
+            System.out.printf("%-4d|%-15s|%-30s|%-5d|%-20s|%-5.2f|",student.getId(), student.getName(), student.getEmail(), student.getAge(), student.getMajor(), student.getGpa());
+            System.out.println();
+        }
+        System.out.println("====|===============|==============================|=====|===============|=====|");
+        System.out.println();
     }
 
 }
